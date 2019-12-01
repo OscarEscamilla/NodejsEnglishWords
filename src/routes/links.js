c10b76d706143b3d71e6f380799dd7c0b39a4c5b
unknown <ozkarezkamiya26qgmail.com>
2019-12-01 15:55:13 -0600

    deploy heroku

--- src/routes/links.js
+++ src/routes/links.js
@@ -10,7 +10,7 @@ const helpers = require('../lib/helpers.js');
 
 //precede de el prefijo /links configurado en app.js 
 //         /links
-router.get('/', helpers.isLoggedIn,controller.list);
+router.get('/', helpers.isLoggedIn ,controller.list);
 
 //renderiza el frmulario para agregar un nuevo link
 router.get('/add', helpers.isLoggedIn, controller.form_add);
